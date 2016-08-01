9c620844e16efdef6b495aced433283ccd4ed236
Laura <l4depende@gmail.com>
2016-08-01 21:42:08 +0100

    Base

--- MipMapGL/main.c
+++ MipMapGL/main.c
@@ -7,9 +7,21 @@
 //
 
 #include <stdio.h>
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc,  char * argv[]) {
+
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1280, 720);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    
+    glutCreateWindow("MipMap");
+    InitGL();
+    glutDisplayFunc(Display); // registra la funcion de rendering
+    glutSpecialFunc(Teclas);
+    
+    glutMainLoop();
     return 0;
 }
